c6a795e234febe2e6c55e5e81fbbcef5ab0b5014
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2025-03-05 09:05:44 +0100

    On ajoute le compte de votes

--- Best_rated_movies.py
+++ Best_rated_movies.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, StringType, DateType
 import json
 from pathlib import Path
+from pyspark.sql.functions import desc, col, count
 
 
 def get_spark() -> SparkSession:
@@ -20,12 +21,7 @@ def read_json_with_schema(data_file_paths: list[str], schema: StructType) -> Dat
     """
     Read JSON files using a predefined schema and return the DataFrame.
     """
-    # Lire les fichiers JSON en utilisant le schéma prédéfini
-    df = spark.read.schema(schema).json(data_file_paths)
-    
-    # Afficher le schéma
-    df.printSchema()
-    
+    df = spark.read.schema(schema).json(data_file_paths)    
     return df
 
 
@@ -43,13 +39,8 @@ assert Path(fichier_json_1).exists(), f"{fichier_json_1} does not exist"
 DATA_FILE_PATHS = [fichier_json_1]
 DF_RAW = read_json_with_schema(data_file_paths=DATA_FILE_PATHS, schema=schema)
 
-
 assert DF_RAW is not None, "Returned result is None"
 assert isinstance(DF_RAW, DataFrame), "Returned result is not a valid DataFrame"
-
-
-print(f"DF_RAW.count(): {DF_RAW.count()}")
-
 
 def compute_average_rating(df: DataFrame) -> DataFrame:
     """
@@ -60,7 +51,12 @@ def compute_average_rating(df: DataFrame) -> DataFrame:
     
     return df_avg
 
+
 DF_AVG_RATING = compute_average_rating(DF_RAW)
+
+DF_RATING_COUNT = DF_RAW.groupBy("item_id").count().withColumnRenamed("count", "count_rating")
+DF_AVG_WITH_COUNT = DF_AVG_RATING.join(DF_RATING_COUNT, on="item_id", how="inner")
+
 
 
 schema_metadata = StructType([
@@ -79,14 +75,10 @@ assert Path(fichier_json_2).exists(), f"{fichier_json_2} does not exist"
 DATA_FILE_PATHS_2 = [fichier_json_2]
 DF_RAW_2 = read_json_with_schema(data_file_paths=DATA_FILE_PATHS_2, schema=schema_metadata)
 
-from pyspark.sql.functions import col
 
-
-df_best_rated_movies = DF_AVG_RATING.join(
+df_best_rated_movies = DF_AVG_WITH_COUNT.join(
     DF_RAW_2, on="item_id", how="inner"
-).select("title", "avg_rating")
+).select("title", "avg_rating", "count_rating")
 
-from pyspark.sql.functions import desc
-
-df_best_rated_movies_sorted = df_best_rated_movies.orderBy(desc("avg_rating"))
+df_best_rated_movies_sorted = df_best_rated_movies.orderBy(desc("avg_rating"), desc("count_rating"))
 df_best_rated_movies_sorted.show(10, truncate=False)
